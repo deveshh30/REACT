7c7a33e0a0ff85587b0279c9d67fa7bd5369f1bb
deveshh30 <devesh30gupta@gmail.com>
2025-12-07 00:15:14 +0530

    completed the job portal

---
+++ ui proj/src/components/page1/page1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page1 = () => {
+    return ( 
+        <>
+        <div className='bg-blue-900 h-screen w-screen'>box1</div>
+        <div className='bg-black h-scree w-screen'>DEvESH</div>
+
+        </>
+     );
+}
+ 
+export default page1;
