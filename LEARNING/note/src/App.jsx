a2ef5200411ba41a04e01c0d7732597e3bb92238
deveshh30 <devesh30gupta@gmail.com>
2025-12-08 15:59:34 +0530

    adding yellow notepad

--- LEARNING/note/src/App.jsx
+++ LEARNING/note/src/App.jsx
@@ -8,26 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-black h-screen w-full">
+      <form className='flex flex-col justify-between text-white p-10'>
+        <div className="flex flex-col p-10 gap-4">
+        <input type="text" placeholder='Enter the heading' className='p-5 border-2 border-amber-500 rounded-2xl' />
+        <br></br>
+        <textarea className='p-5 border-2 border-amber-300 rounded-2xl' name="" placeholder='Describe your heading' id=""></textarea>
+        <button className='bg-white text-5xl text-black py-10 m-10 mt-20 border-6 border-gray-600 rounded-b-md'>Add Notes</button>
+
+        </div>
+
+        <img className='h-40 w-40' src="https://imgs.search.brave.com/xFNevK0Uk5HJWr11F7E_dn9Se85IIOSnrMxHua5ApKw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMTEv/MTAzLzU2NS9zbWFs/bC9zcXVhcmUtc3Rp/Y2t5LW5vdGUtcG5n/LnBuZw" alt="" />
+              </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
