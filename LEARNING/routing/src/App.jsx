cea23d1081050a12e262bd03a29e13ce64b6bad4
deveshh30 <devesh30gupta@gmail.com>
2025-12-11 20:54:31 +0530

    changes

--- LEARNING/routing/src/App.jsx
+++ LEARNING/routing/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from './componennts/Navbar'
 import Footer from './componennts/Footer'
 import { Route, Routes } from 'react-router-dom'
 import About from './page/About'
 import Product from './page/Product'
 import Contact from './page/Contact'
+import Men from './page/Men'
+import Women from './page/Women'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,12 +16,12 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<About />} />
-        <Route path='/Product' element={<Product />} />
-        <Route path='/contact' element={<Contact />} /> 
-        {/* path is non-case-senstive */}
+        <Route path='/product' element={<Product />} />
+        <Route path='/contact' element={<Contact />} />
+
+        <Route path='/product/men' element={<Men />} />
+        <Route path='/product/women' element={<Women />} />
       </Routes>
-
-
       <Footer/>
     </>
   )
